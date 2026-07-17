8d4e621478e41b7ccf41e46272137a7fb5d024a8
Sachin <141260725+sachin7444@users.noreply.github.com>
2026-07-17 20:39:09 +0530

    Update

--- api/routes/test.route.js
+++ api/routes/test.route.js
@@ -1,10 +1,14 @@
-import express from 'express'
+import express from 'express';
 import { shouldBeAdmin, shouldBeLoggedIn } from '../controllers/test.controller.js';
 import { verifyToken } from '../middleware/verifyToken.js';
+
 const router = express.Router();
 
-router.get("/should-be-logged-in", verifyToken, shouldBeLoggedIn);
+router.get("/", (req, res) => {
+  res.send("API is working!");
+});
 
+router.get("/should-be-logged-in", verifyToken, shouldBeLoggedIn);
 router.get("/should-be-admin", shouldBeAdmin);
 
 export default router;
